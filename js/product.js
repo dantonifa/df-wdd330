9bee11a178b108f085575d85eb90076d3aeb0240
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2025-11-29 02:03:08 -0600

    Refactor product.js for improved parameter handling
    Refactor product ID retrieval and add logging for product details. Introduce getParam function to extract URL parameters.

--- js/product.js
+++ js/product.js
@@ -3,9 +3,19 @@ import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
 loadHeaderFooter();
-
 const dataSource = new ProductData("tents");
-const productID = getParam("product");
-
-const product = new ProductDetails(productID, dataSource);
+const productId = getParam("product");
+const product = new ProductDetails(productId, dataSource);
+console.log(dataSource.findProductById(productId));
 product.init();
+/*Get the product parameter, the product id, from the URL using the helper function getParam.*/
+export function getParam(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get("product");
+  const params = new URLSearchParams(window.location.search);
+  const productId= urlParams.get("productId");
+  return params.get(param);
+}
+/*Create an instance of ProductDate data class with the URL 
+it should use to look for products.*/
