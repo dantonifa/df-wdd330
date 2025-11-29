ce9880f4eb8d3aa0445ead8fca8ba4d3607bdbc6
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2025-11-29 02:06:07 -0600

    Refactor getParam function for clarity and consistency

--- js/utils.mjs
+++ js/utils.mjs
@@ -26,8 +26,9 @@ export function setClick(selector, callback) {
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get(param);
-  return product
+  const product = urlParams.get("product");
+  const params = new URLSearchParams(window.location.search);
+  return params.get(param);
 }
 
 export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
