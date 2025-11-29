86bb813440e0258ac53598aa95612aa376a50491
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2025-11-29 01:47:50 -0600

    Refactor comments and add path property to ProductDetails
    Updated comments for clarity and added a path property.

--- js/ProductDetails.mjs
+++ js/ProductDetails.mjs
@@ -1,20 +1,22 @@
 import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 
 export default class ProductDetails {
-
   constructor(productId, dataSource) {
     this.productId = productId;
     this.product = {};
     this.dataSource = dataSource;
+    this.path = `../json/${this.category}.json`;
   }
 
   async init() {
-    // use the datasource to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+    // use the datasource to get the details for the current product. 
+    // findProductById will return a promise! use await or .then() to process it
     this.product = await this.dataSource.findProductById(this.productId);
     // the product details are needed before rendering the HTML
     this.renderProductDetails();
     // once the HTML is rendered, add a listener to the Add to Cart button
-    // Notice the .bind(this). This callback will not work if the bind(this) is missing. Review the readings from this week on "this" to understand why.
+    // Notice the .bind(this). This callback will not work if the bind(this) 
+    // is missing. Review the readings from this week on "this" to understand why.
     document
       .getElementById("add-to-cart")
       .addEventListener("click", this.addProductToCart.bind(this));
@@ -49,3 +51,4 @@ function productDetailsTemplate(product) {
 
   document.querySelector("#add-to-cart").dataset.id = product.Id;
 }
+
