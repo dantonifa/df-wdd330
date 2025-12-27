a0fb80da8c2e53a7e1c898ef6f1c0773503a1f83
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2025-12-27 12:28:05 -0600

    Fix import path and call loadHeaderFooter function

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 /*main.js*/
-/*Import the loadHeaderFooter function from utils.mjs*/
-import {loadHeaderFooter} from "js/utils.mjs";
+/*Import the loadHeaderFooter function from utils. mjs*/
+import {loadHeaderFooter} from "./utils.mjs";
+
 /*call that imported function to load the header and footer */
-//create an async function to load the header and footer
-async function loadHeaderFooterAsync() {
- const header=document.querySelector("#main-header");
- const footer=document.querySelector("#main-footer");
-}   
-loadHeaderFooterAsync();
+loadHeaderFooter();
