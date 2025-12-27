5f481102b8e787f8a1a4986c98888c9c2acc16be
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2025-12-27 08:34:25 -0600

    Fix typo in footer template path

--- js/utils.mjs
+++ js/utils.mjs
@@ -82,7 +82,7 @@ async function loadTemplate(path) {
 
 export async function loadHeaderFooter() {
   const headerTemplate = await loadTemplate("/partials/header.html");  // Note the leading /
-  const footerTemplate = await loadTemplate("/partials/footer. html");  // Note the leading /
+  const footerTemplate = await loadTemplate("/partials/footer.html");  // Note the leading /
   const headerElement = document. querySelector("#main-header");
   const footerElement = document.querySelector("#main-footer");
   renderWithTemplate(headerTemplate, headerElement);
